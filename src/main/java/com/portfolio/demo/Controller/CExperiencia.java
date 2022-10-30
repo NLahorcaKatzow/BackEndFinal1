e47d007c7487e2397b4837644364bae4dac00e4a
NLahorcaKatzow <nicolas.lahorca.k@gmail.com>
2022-10-30 19:50:01 -0300

    correccion de errores

--- src/main/java/com/portfolio/demo/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/demo/Controller/CExperiencia.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("explab")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfoliolahnic.web.app/")
 //@CrossOrigin(origins = "")
 public class CExperiencia {
 
